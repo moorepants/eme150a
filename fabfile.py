c40760648a26d764afcf0d78a9a042c855c64c52
Jason K. Moore <moorepants@gmail.com>
2015-09-23 17:13:16 -0700

    Added nojekyll file.

--- fabfile.py
+++ fabfile.py
@@ -90,5 +90,5 @@ def publish():
 def gh_pages():
     """Publish to GitHub Pages"""
     rebuild()
-    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
+    local("ghp-import -n -b {github_pages_branch} {deploy_path}".format(**env))
     local("git push origin {github_pages_branch}".format(**env))
