f25542563899a5c9d83543f0c0b78e0fb0b5d4a5
Jason K. Moore <moorepants@gmail.com>
2016-09-13 13:06:08 -0700

    Removed -website from url.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://moorepants.github.io/eme150a-website'
+SITEURL = 'http://moorepants.github.io/eme150a'
 RELATIVE_URLS = False
 
 FEED_ATOM = None
